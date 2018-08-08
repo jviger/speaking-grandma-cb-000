4652da8c143dee489ecec238d0b0ff559002eb84
Jason Viger <jason.viger@gmail.com>
2018-08-08 06:03:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU TOO GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else return "NO, NOT SINCE 1938!"
   end
